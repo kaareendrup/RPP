a824ceeb67e09c37c9b23b14dd6b150c8de9fe3c
Kaare Endrup <kaare.endrup@gmail.com>
2023-04-09 19:30:24 +0200

    Supply test function

--- tests/classification_test.py
+++ tests/classification_test.py
@@ -15,13 +15,17 @@ db = TEST_DATA_DIR+'/test_database.db'
 
 FlavourPlotter = ClassificationPlotter('test_model', plot_dir, 'v_e', 'v_u')
 
-assert(FlavourPlotter._target_label == r"$\nu_e$")
-assert(FlavourPlotter._background_label == r"$\nu_u$")
+def test_classification_plotter_init():
+
+    assert(FlavourPlotter._target_label == r"$\nu_e$")
+    assert(FlavourPlotter._background_label == r"$\nu_u$")
 
 
 FlavourPlotter.add_results(results, 'test_model', db, reverse=True, cut_functions=[InteractionTypeCutter('cc')])
 
-assert(FlavourPlotter._models_list[0]._label == r'test_model')
-assert(FlavourPlotter._models_list[0]._db_name == 'test_database')
+def test_add_model():
 
-assert(FlavourPlotter._models_list[0]._predictions[0] == 1 - pd.read_csv(results)['v_u_pred'][1])
+    assert(FlavourPlotter._models_list[0]._label == r'test_model')
+    assert(FlavourPlotter._models_list[0]._db_name == 'test_database')
+
+    assert(FlavourPlotter._models_list[0]._predictions[0] == 1 - pd.read_csv(results)['v_u_pred'][1])
